66b5a42b2e2649dc1fe9565d4a6f413ea9fd17e1
Rorurmdara <rouermdara9200@gmail.com>
2026-04-20 14:58:59 +0700

    dara

--- cambodia-travel-blog/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.html", "./src/js/**/*.js"],
-  darkMode: false,
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
